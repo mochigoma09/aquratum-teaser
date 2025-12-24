8409c01b962802a9d7abd34486f12df814734ad6
m_goto <m_goto@amy-creative.jp>
2025-12-24 23:46:38 +0900

    feat: Enable HTML5 history mode routing with server rewrite rules and 404 fallback.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import MemberDetail from '../views/MemberDetail.vue';
 
@@ -34,7 +34,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory('/aquratum/'),
+    history: createWebHistory('/aquratum/'),
     routes,
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
