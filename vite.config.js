7deb1faeb8601894fb0c8fa5888d9a0ea55a0ac7
m_goto <m_goto@amy-creative.jp>
2025-12-24 23:21:43 +0900

    feat: Add `vite-plugin-image-optimizer` for image optimization during build.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+ 
+ // https://vite.dev/config/
+ export default defineConfig({
+   plugins: [
+     vue(),
+     ViteImageOptimizer({
+       /* options */
+     }),
+   ],
+ })
