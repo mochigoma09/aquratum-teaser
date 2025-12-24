36476dab64643f0ae4c99b1b867233c962a7d3b5
m_goto <m_goto@amy-creative.jp>
2025-12-24 23:38:28 +0900

    refactor: Remove image optimizer and its dependencies, and add 404.html copy to build script.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // ViteImageOptimizer({
-    //   png: {
-    //     quality: 100,
-    //   },
-    //   jpeg: {
-    //     quality: 100,
-    //   },
-    //   webp: {
-    //     lossless: true,
-    //   },
-    // }),
   ],
   base: '/aquratum/',
 })
